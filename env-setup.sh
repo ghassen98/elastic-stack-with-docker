1632d63bec2a228139f526ee34846d172aa5f57a
GHassen <ghassen.aouinti@gmail.com>
2020-04-13 01:39:54 +0200

    Update env-setup.sh

--- env-setup.sh
+++ env-setup.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
 case "$1" in
-    init)
-        echo "Unpacking malware-traffic-analysis.net data..."
-        bunzip2 -k malware-traffic-analysis.net/eve.json.bz2
-        docker-compose build
-        docker-compose up
-        rm malware-traffic-analysis.net/eve.json
+    go)
+        echo "Removing old versions of Docker ..."
+        sudo apt-get remove docker docker-engine docker.io containerd runc
+
+        sudo apt-get update
+        
+        echo "Installing Docker ..."
+        
+        sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+        sudo apt-key fingerprint 0EBFCD88
+
+        sudo add-apt-repository \
+            "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) \
+            stable"
+
+        sudo apt-get update
+
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+
+        echo "Installing Docker machine ..."
+
+        base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+            curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+            sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+            chmod +x /usr/local/bin/docker-machine
+        
+        echo "Docker Machine version : "
+        docker-machine version
+
+        echo "Installing Docker-compose ..."
+
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+        sudo chmod +x /usr/local/bin/docker-compose
+
+        echo "Docker-compose version : "
+        docker-compose --version
         ;;
     init-windowsonly)
         docker-compose build
@@ -22,7 +57,7 @@ case "$1" in
     *)
         cat <<HELP;;
 Usage:
-$0 init: run environment and import data.
+$0 go: install docker, docker machine and docker-compose.
 $0 run: run already initialized environment.
 $0 rm: discard the environment (frees disk space, has to be recreated with init command)
 HELP
